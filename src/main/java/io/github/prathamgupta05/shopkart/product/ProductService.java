df13279b9cc662622eaad5f0ccb0adbce1f959e9
Pratham Gupta <guptadivyravindra@gmail.com>
2025-04-10 11:54:58 +0530

    create addProduct()

---
+++ src/main/java/io/github/prathamgupta05/shopkart/product/ProductService.java
@@ -0,0 +1,27 @@
+package io.github.prathamgupta05.shopkart.product;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+@Service
+public class ProductService {
+    private List<Product> productList;
+
+    public ProductService() {
+        productList = new LinkedList<>();
+    }
+
+    //CRUD - CREATE READ UPDATE DELETE
+    //CREATE
+    public Product addProduct(Product product) {
+       productList.add(product);
+       return product;
+    }
+    //READ
+    //UPDATE
+    //DELETE
+}
