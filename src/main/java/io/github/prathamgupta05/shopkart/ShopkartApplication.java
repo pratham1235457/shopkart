0ba591e4694dd2adc439b49a1a43c50a59493296
Pratham Gupta <guptadivyravindra@gmail.com>
2025-04-10 09:52:10 +0530

    initial commit

---
+++ src/main/java/io/github/prathamgupta05/shopkart/ShopkartApplication.java
@@ -0,0 +1,13 @@
+package io.github.prathamgupta05.shopkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopkartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopkartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/prathamgupta05/shopkart/ShopkartApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.prathamgupta05.shopkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopkartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
